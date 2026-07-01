16384b496110bcc1ec9af4d7cacb33865b9638b2
platonic <aaravraj25.2010@gmail.com>
2026-07-01 23:44:03 +0530

    Time: 10 ms (92.17%), Space: 12.3 MB (56.08%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -5,21 +5,20 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        result = ""
+        result=[]
         i=0
         j=0
-        while(i<len(word1)and j<len(word2)):
-            result+=word1[i]
-            result+=word2[j]
+        while(i<len(word1) and i<len(word2)):
+            result.append(word1[i])
+            result.append(word2[j])
             i+=1
             j+=1
-                 
-            
-        while(j<len(word2)):
-            result+=word2[j]
-            j+=1
-        
         while(i<len(word1)):
-            result+=word1[i]
+            result.append(word1[i])
             i+=1
-        return result
+        while(j<len(word2)):
+            result.append(word2[j])
+            j+=1
+        return"".join(result)
+
+
