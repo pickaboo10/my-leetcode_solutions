f0af448e66a95dc28ecbaecea4cc1830dd9fa673
platonic <aaravraj25.2010@gmail.com>
2026-08-09 15:04:54 +0530

    Time: 14 ms (76.32%), Space: 12.7 MB (62.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = ''.join(char.lower()for char in s if char.isalnum())
-        p = s[::-1]
-        if s == p:
-            return True
-        else:
-            return False 
+        i = 0
+        j = len(s) - 1
+        while(i<j):
+            while i<j and not s[i].isalnum():
+                i+=1
+            while i<j and not s[j].isalnum():
+                j-=1
+
+            if s[i].lower()!= s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True 
