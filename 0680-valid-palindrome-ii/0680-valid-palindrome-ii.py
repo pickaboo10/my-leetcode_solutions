7ae72d68e8e9e785af01cf76ec1e7afacdd03f69
platonic <aaravraj25.2010@gmail.com>
2026-08-09 16:39:58 +0530

    Time: 91 ms (18.2%), Space: 12.6 MB (72.78%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -4,29 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        i = 0
-        j = len(s)-1
-        while i<j:
-            if s[i]==s[j]:
+        def isPalindrome(i,j):
+            while(i<j):
+                if s[i]!= s[j]:
+                    return False
                 i+=1
                 j-=1
-            else:
-                left = i+1
-                right = j
-                skipleft = True
-                while(left<right):
-                    if s[left]!= s[right]:
-                        skipleft = False
-                        break
-                    left+=1
-                    right-=1
-                left = i
-                right = j-1
-                skipright = True
-                while(left<right):
-                    if s[left]!= s[right]:
-                        skipright = False
-                    left+=1
-                    right-=1
-                return skipleft or skipright
+
+            return True
+        i = 0
+        j = len(s)-1
+        while(i<j):
+            if s[i]!= s[j]:
+                return isPalindrome(i+1, j) or isPalindrome(i, j-1)
+            i+=1
+            j-=1
         return True
