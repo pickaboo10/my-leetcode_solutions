d33a3a3115ec673fc485a092a416b432199dbe10
platonic <aaravraj25.2010@gmail.com>
2026-09-22 22:01:54 +0530

    Time: 119 ms (35.82%), Space: 18.9 MB (76.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,9 +5,9 @@ class Solution(object):
         :rtype: int
         """
         i = prices[0]
-        diff = 0
-        for j in range(1, len(prices)):
-            sub = prices[j] - i
-            diff = max(diff, sub)
-            i = min(i, prices[j])
-        return diff
+        j = 0
+        for x in prices:
+            i = min(i,x)
+            j = max(j, x-i)
+        return j
+            
